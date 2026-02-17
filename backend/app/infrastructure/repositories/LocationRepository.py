2ca738d2a04f577be9096029b1d9441c30c1b1c8
OierAbrisqueta <oabrisketa76@gmail.com>
2026-02-17 19:06:17 +0100

    LocationRepository.py created

---
+++ backend/app/infrastructure/repositories/LocationRepository.py
@@ -0,0 +1,78 @@
+import json
+from pathlib import Path
+from typing import List, Dict, Optional
+from functools import lru_cache
+
+class LocationRepository:
+
+    instance = None
+    locations: List[Dict] = []
+    loaded: bool = False
+
+    def __new__(cls) -> 'LocationRepository':
+        if cls.instance is None:
+            cls.instance = super().__new__(cls)
+        return cls.instance
+
+    def __init__(self, path: str = "data/locations.json"):
+        if not self.loaded:
+            self.file_path = Path(path)
+            self.load_locations()
+
+    def load_locations(self) -> None:
+        try:
+            with open(self._file_path, 'r', encoding='utf-8') as f:
+                self.locations = json.load(f)
+            self.loaded = True
+        except FileNotFoundError:
+            raise FileNotFoundError("Archivo no encontrado")
+        except json.JSONDecodeError as e:
+            raise ValueError("Error al parsear JSON")
+
+    def get_locations(self):
+        return self.locations
+
+    def get_by_zone(self, zone: str):
+        return [loc for loc in self.locations if loc.get('zone') == zone]
+
+    def get_by_tags_all(self, tags: list[str]):
+        non_repeated = set(tags)
+        devolver = []
+
+        for loc in self.locations:
+            loc_tags = loc.get("tags")
+            all: bool = True
+            for tag in non_repeated:
+                if tag not in loc_tags:
+                    all = False
+                    break
+            if all:
+                devolver.append(loc)
+
+        return devolver
+
+    def get_by_tags_any (self, tags: list[str]):
+        non_repeated = set(tags)
+        devolver = []
+
+        for loc in self.locations:
+            loc_tags = loc.get("tags")
+            any: bool = False
+            for tag in non_repeated:
+                if loc_tags:
+                    any = True
+                    break
+            if any:
+                devolver.append(loc)
+
+        return devolver
+
+    def get_by_id(self, id: str):
+        for loc in self.locations:
+            if loc.get("id") == id:
+                return loc.copy()
+        return None
+
+    def reload(self):
+        self.loaded = False
+        self.load_locations()
